dd93cba46101d11538d2e40174c74bf8705e9112
inlak16 <53479637+inlak16@users.noreply.github.com>
2020-12-20 22:43:46 +0100

    Update get_rewards_apollo.py

--- Code-Snippets/get_rewards_apollo.py
+++ Code-Snippets/get_rewards_apollo.py
@@ -1,3 +1,4 @@
+
 import requests
 import os
 import csv
@@ -6,51 +7,24 @@ from time import gmtime, strftime
 os.system('ls')
 
 #enter your home directory with your username
-home = "/home/yourUserName/"
+home = "/home/myusername/"
 
-#enter your node(s) and set active slots below to 1! Slot 1 is always active!
+#enter your apollo node(s) in the brackets, seperated by comma and surrounded by inverted commas like this ['0x21...','0x35...','0x64...']
 
-node1 = '0x0000000000000000000000000000000000000000'
-node2 = '0x0000000000000000000000000000000000000000'
-node3 = '0x0000000000000000000000000000000000000000'
-node4 = '0x0000000000000000000000000000000000000000'
-node5 = '0x0000000000000000000000000000000000000000'
-node6 = '0x0000000000000000000000000000000000000000'
+apollonodes = []
 
-node2active = 0
-node3active = 0
-node4active = 0
-node5active = 0
-node6active = 0
 
 #currency to write price into the csv file (for example EUR, CAD, CHF, CNY, RUB, JPY, for available conversions check: https://www.coingecko.com/)
 currency = 'USD'
 
 #############################################################
 
-nodes = [node1]
-
-if node2active == 1:
-    nodes.append(node2)
-
-if node3active == 1:
-    nodes.append(node3)
-
-if node4active == 1:
-    nodes.append(node4)
-
-if node5active == 1:
-    nodes.append(node5)
-
-if node6active == 1:
-    nodes.append(node6)
-
 api_url_base = 'https://explorer-api.ambrosus.com/apollos'
 count = 0
-state = ["a","b","c","d","e","f"]
-status = ["a","b","c","d","e","f"]
-balance = ["a","b","c","d","e","f"]
-for each in nodes:
+state = []
+status = []
+balance = []
+for each in apollonodes:
     print(each)
     response = requests.get(api_url_base)
     data = str(response.json())
@@ -60,7 +34,7 @@ for each in nodes:
         next = (data.split('next'))
         if (len(next)) < 2:
             status[count] = "OFFLINE"
-            balance[count] = 0
+            balance.append = 0
             break
         else:
             pageCloseEnd = (next[1].split(','))
@@ -79,18 +53,18 @@ for each in nodes:
     evenCloserBalance = (closerBalance[1].replace("\"", ""))
     evenCloserBalance = (evenCloserBalance.replace("}", ""))
     #balanceRealClose = (evenCloserBalance.split('.'))
-    balance[count] = evenCloserBalance
+    balance.append(evenCloserBalance)
 
     closerState = (found[1].split('state'))
     evenCloserState = (closerState[1].split(','))
     evenCloserStatenow = (evenCloserState[0].split(': '))
-    state[count] = (evenCloserStatenow[1].replace("\"", ""))
+    state.append(evenCloserStatenow[1].replace("\"", ""))
     state[count] = (state[count].replace("'", "")) 
 
     closerStatus = (found[1].split('status'))
     evenCloserStatus = (closerStatus[1].split(","))
     evenCloserStatusnow = (evenCloserStatus[0].split(": "))
-    status[count] = (evenCloserStatusnow[1].replace("\"", ""))
+    status.append(evenCloserStatusnow[1].replace("\"", ""))
     status[count] = (status[count].replace("'", ""))
     
     if state[count] == "RETIRED":
@@ -116,62 +90,74 @@ if 'buffer' in locals():
 if 'buffer' in globals():
     buffer.clear()
 
-buffer = [str(balance[0])+"\n"]
+count = 0
+buffer = []
+for each in apollonodes:
+    buffer.append(str(balance[count])+"\n")
+    count = count + 1
 
-if node2active == 1:
-    buffer.append(str(balance[1])+"\n")
+reset = "0"
 
-if node3active == 1:
-    buffer.append(str(balance[2])+"\n")
+#function to check if buffer file exists, set it up if necessary.
+def buffer_exist(bfile,nodetype):
+    val = ""
+    try:
+        f = open(home+bfile)
+    except IOError:
+        print("File not accessible creating "+bfile+" !")
+        global reset
+        reset = "1"
+        f = open(home+bfile,"w")
+        count = 0
+        for each in nodetype:
+            if count == len(nodetype)-1:
+                val = (val+"0")
+            else:
+                val = (val+"0\n")
+            count = count + 1
+        f.writelines(val)
+    finally:
+        f.close()
 
-if node4active == 1:
-    buffer.append(str(balance[3])+"\n")
+#function to check if nodes are correctly registered in buffer file, reset if needed and read.
+def buffer_slots(bfile,nodetype,nodename,type):
+    f = open(home+bfile,"r")
+    readtxt = f.readlines()
+    f.close()
+    val = ""
+    if len(nodetype) != len(readtxt):
+        print("Number of "+nodename+" has changed, resetting "+type+" file!")
+        global reset
+        reset = "1"
+        f = open(home+bfile,"w")
+        count = 0
+        for each in nodetype:
+            if count == len(nodetype)-1:
+                val = (val+"0")
+            else:
+                val = (val+"0\n")
+            count = count + 1
+        f.writelines(val)
+        f.close()
+        return reset
 
-if node5active == 1:
-    buffer.append(str(balance[4])+"\n")
-
-if node6active == 1:
-    buffer.append(str(balance[5])+"\n")
+buffer_exist("buffer.txt",apollonodes)
+buffer_slots("buffer.txt",apollonodes,"Apollo Nodes","buffer")
 
 bufffile = open(home+"buffer.txt","r")
 buff = bufffile.readlines()
 bufffile.close()
-buff[0] = (buff[0].replace("\n", ""))
-baldif0 = float(balance[0]) - float(buff[0])
-if node2active == 1:
-    buff[1] = (buff[1].replace("\n", ""))
-    baldif1 = float(balance[1]) - float(buff[1])
-if node3active == 1:
-    buff[2] = (buff[2].replace("\n", ""))
-    baldif2 = float(balance[2]) - float(buff[2])
-if node4active == 1:
-    buff[3] = (buff[3].replace("\n", ""))
-    baldif3 = float(balance[3]) - float(buff[3])
-if node5active == 1:
-    buff[4] = (buff[4].replace("\n", ""))
-    baldif4 = float(balance[4]) - float(buff[4])
-if node6active == 1:
-    buff[4] = (buff[5].replace("\n", ""))
-    baldif5 = float(balance[5]) - float(buff[5])
+
+count = 0
+csvfile = open(home+"rewards.csv","a")
+for each in apollonodes:
+    buff[count] = (buff[count].replace("\n", ""))
+    baldif = (float(balance[count]) - float(buff[count]))
+    csv = ("\"Staking\",\""+str(baldif)+"\",\"AMB\",\"\",\"\",\"\",\"\",\"\",\"\",\"Apollo Node"+str(count+1)+"\",\""+datewrite+"\",\""+priceusd+"\"\n")
+    csvfile.writelines(csv)
+    count = count + 1
+csvfile.close()
+
 bufffil = open(home+"buffer.txt","w")
 bufffil.writelines(buffer)
 bufffil.close()
-csvfile = open(home+"rewards.csv","a")
-csv0 = ("\"Staking\",\""+str(baldif0)+"\",\"AMB\",\"\",\"\",\"\",\"\",\"\",\"\",\"Apollo Node1\",\""+datewrite+"\",\""+priceusd+"\"\n")
-csvfile.writelines(csv0)
-if node2active == 1:
-    csv1 = ("\"Staking\",\""+str(baldif1)+"\",\"AMB\",\"\",\"\",\"\",\"\",\"\",\"\",\"Apollo Node2\",\""+datewrite+"\",\""+priceusd+"\"\n")
-    csvfile.writelines(csv1)
-if node3active == 1:
-    csv2 = ("\"Staking\",\""+str(baldif2)+"\",\"AMB\",\"\",\"\",\"\",\"\",\"\",\"\",\"Apollo Node3\",\""+datewrite+"\",\""+priceusd+"\"\n")
-    csvfile.writelines(csv2)
-if node4active == 1:
-    csv3 = ("\"Staking\",\""+str(baldif3)+"\",\"AMB\",\"\",\"\",\"\",\"\",\"\",\"\",\"Apollo Node4\",\""+datewrite+"\",\""+priceusd+"\"\n")
-    csvfile.writelines(csv3)
-if node5active == 1:
-    csv4 = ("\"Staking\",\""+str(baldif4)+"\",\"AMB\",\"\",\"\",\"\",\"\",\"\",\"\",\"Apollo Node5\",\""+datewrite+"\",\""+priceusd+"\"\n")
-    csvfile.writelines(csv4)
-if node6active == 1:
-    csv5 = ("\"Staking\",\""+str(baldif5)+"\",\"AMB\",\"\",\"\",\"\",\"\",\"\",\"\",\"Apollo Node6\",\""+datewrite+"\",\""+priceusd+"\"\n")
-    csvfile.writelines(csv5)
-csvfile.close()
